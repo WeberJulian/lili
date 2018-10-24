a0e091e2e6b81e79c5f4880ccf2d383c9aa7baa7
Julian WEBER <julian.weber@hotmail.fr>
2018-10-24 19:57:51 +0200

    Enhanced camera functions

--- Screens/PhotoEditeur.js
+++ Screens/PhotoEditeur.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import { Camera, Permissions } from 'expo';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 
 export default class Editeur extends Component {
 	static navigationOptions = {
-		title: 'Scanne un texte !'
+		title: 'Scanne un texte !',
+		photo: ''
 	};
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			hasCameraPermission: null,
-			type: Camera.Constants.Type.back
+			type: Camera.Constants.Type.back,
+			flash: false,
+			photo: ''
 		};
 	}
 
@@ -21,16 +24,40 @@ export default class Editeur extends Component {
 		this.setState({ hasCameraPermission: status === 'granted' });
 	}
 
+	switchFlash() {
+		this.setState((state) => ({ flash: !state.flash }));
+		console.log(this.state.flash);
+	}
+
+	snap = async () => {
+		if (this.camera) {
+			let photo = await this.camera.takePictureAsync();
+			console.log(JSON.stringify(photo));
+			this.setState({ photo: photo.uri });
+		}
+	};
+
+	retake(){
+		this.setState({photo: ""})
+	}
+
 	render() {
 		const { hasCameraPermission } = this.state;
 		if (hasCameraPermission === null) {
 			return <View />;
 		} else if (hasCameraPermission === false) {
 			return <Text>No access to camera</Text>;
-		} else {
+		} else if (this.state.photo == '') {
 			return (
 				<View style={{ flex: 1 }}>
-					<Camera style={{ flex: 1 }} type={this.state.type} flashMode="torch">
+					<Camera
+						style={{ flex: 1 }}
+						type={this.state.type}
+						flashMode={this.state.flash ? 'torch' : 'off'}
+						ref={(ref) => {
+							this.camera = ref;
+						}}
+					>
 						<View
 							style={{
 								flex: 1,
@@ -39,7 +66,35 @@ export default class Editeur extends Component {
 							}}
 						>
 							<View style={{ flex: 1, justifyContent: 'space-between' }}>
-								<View style={{ flex: 1 }} />
+								<View
+									style={{
+										flex: 1,
+										flexDirection: 'row-reverse',
+										alignItems: 'center'
+									}}
+								>
+									<View>
+										<TouchableOpacity onPress={this.switchFlash.bind(this)}>
+											<View
+												style={{
+													height: 40,
+													width: 40,
+													backgroundColor: 'orange',
+													justifyContent: 'center',
+													alignItems: 'center',
+													borderRadius: 30,
+													margin: 10
+												}}
+											>
+												{this.state.flash ? (
+													<MaterialIcons name="flash-on" size={20} color="white" />
+												) : (
+													<MaterialIcons name="flash-off" size={20} color="white" />
+												)}
+											</View>
+										</TouchableOpacity>
+									</View>
+								</View>
 								<View style={{ flex: 6 }} />
 								<View
 									style={{
@@ -49,7 +104,7 @@ export default class Editeur extends Component {
 										alignItems: 'center'
 									}}
 								>
-									<TouchableOpacity>
+									<TouchableOpacity onPress={this.snap.bind(this)}>
 										<View
 											style={{
 												height: 60,
@@ -69,6 +124,56 @@ export default class Editeur extends Component {
 						</View>
 					</Camera>
 				</View>
+			);
+		} else {
+			return (
+				<ImageBackground source={{ uri: this.state.photo }} style={{ flex: 1 }}>
+					<View style={{ flex: 1, justifyContent: 'space-between' }}>
+						<View style={{ flex: 1 }} />
+						<View style={{ flex: 6 }} />
+						<View
+							style={{
+								flex: 1,
+								flexDirection: 'row',
+								justifyContent: 'center',
+								alignItems: 'center'
+							}}
+						>
+							<TouchableOpacity onPress={this.retake.bind(this)}>
+								<View
+									style={{
+										height: 60,
+										width: 60,
+										backgroundColor: 'orange',
+										justifyContent: 'center',
+										alignItems: 'center',
+										borderRadius: 30,
+										marginBottom: 10,
+										marginRight: 10
+									}}
+								>
+									<MaterialIcons name="cached" size={30} color="white" />
+								</View>
+							</TouchableOpacity>
+							<TouchableOpacity>
+								<View
+									style={{
+										height: 60,
+										width: 60,
+										backgroundColor: 'orange',
+										justifyContent: 'center',
+										alignItems: 'center',
+										borderRadius: 30,
+										marginBottom: 10,
+										marginLeft: 10
+									}}
+								>
+									<FontAwesome name="check" size={30} color="white" />
+								</View>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</ImageBackground>
 			);
 		}
 	}
