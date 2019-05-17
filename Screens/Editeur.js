f068ffd33b92674d0cd051bbe6e400979165b8e3
Julian WEBER <julian.weber@hotmail.fr>
2019-05-17 20:28:59 +0200

    modified the place of the listen button

--- Screens/Editeur.js
+++ Screens/Editeur.js
@@ -5,32 +5,14 @@ import { Speech } from 'expo';
 import AdaptativeText from '../Components/AdaptativeText';
 import { NavigationEvents } from 'react-navigation';
 import { connect } from 'react-redux'
+import { FAB, Portal } from 'react-native-paper';
+import { withNavigationFocus } from 'react-navigation';
 
 import styles from './Styles/ListeEditeurStyles';
 import { settingsActions } from "../redux/action"
 
-var rate = 12
-var text = ""
 
 class Editeur extends Component {
-	static navigationOptions = ({ navigation }) => {
-		return {
-			headerRight: (
-				<TouchableOpacity
-					onPress={
-						async () => {
-							if (await Speech.isSpeakingAsync()) {
-								Speech.stop()
-							} else {
-								Speech.speak(this.props.text, { language: "fr", rate: rate * 3 })
-							}
-						}
-					}
-				>
-					<MaterialCommunityIcons name="ear-hearing" size={30} style={{ margin: 10 }} />
-				</TouchableOpacity>)
-		};
-	};
 
 	componentWillMount() {
 		this.props.updateText(this.props.navigation.getParam('text', ''))
@@ -39,17 +21,45 @@ class Editeur extends Component {
 	render() {
 		return (
 			<ScrollView>
-				<AdaptativeText text={this.props.text} options={this.props.settings} />
+				<Portal.Host>
+					<AdaptativeText text={this.props.text} options={this.props.settings} />
+				</Portal.Host>
+				<Portal>
+				{this.props.isFocused ? (
+						<View style={{flex: 1, flexDirection: "column-reverse", alignItems: "flex-end", top: 65, margin: 20}}>
+						<FAB
+							style={{ height: 55, width: 55, alignItems: 'center', justifyContent: 'center',}}
+							color="white"
+							theme={{ colors: { accent: 'orange' } }}
+							icon="speaker"
+							onPress={
+								async () => {
+									if (await Speech.isSpeakingAsync()) {
+										Speech.stop()
+									} else {
+										Speech.speak("ceci", { language: "fr", rate: this.props.settings.rate * 3 })
+										Speech.speak("est", { language: "fr", rate: this.props.settings.rate * 3 })
+										Speech.speak("un", { language: "fr", rate: this.props.settings.rate * 3 })
+										Speech.speak("test", { language: "fr", rate: this.props.settings.rate * 3 })
+									}
+								}
+							}
+						/>
+					</View>
+					) : (
+						<View />
+					)}
+				</Portal>
 			</ScrollView>
 		);
 	}
 }
 
-const mapStateToProps = (state) => ({...state})
+const mapStateToProps = (state) => ({ ...state })
 const mapDispatchToProps = (dispatch) => {
 	return {
 		updateText: (text) => dispatch(settingsActions.updateText(text)),
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Editeur)
+export default connect(mapStateToProps, mapDispatchToProps)(withNavigationFocus(Editeur))
